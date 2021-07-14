07f82e8656042d5f7385858d77b8606115b0e1ce
ashleyannlaz <paperpastels@gmail.com>
2021-07-14 14:17:55 -0400

    Finished Guided Project

--- src/components/Details.js
+++ src/components/Details.js
@@ -24,8 +24,8 @@ export default function Details(props) {
   // See what happens if we don't clean up.
 
   useEffect (() => {
-    const listener = evt => {//dirty thing
-      console.log(`Here is a random number ${Math.random()}`)
+    const listener = evt => {//dirty thing, 
+      console.log(`random number ${(Math.random())}`)
     }
     document.addEventListener('click',listener)
     // memory leak after you close the see details it will keep returning a number
@@ -38,10 +38,29 @@ export default function Details(props) {
   },[])
   // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
 
+  useEffect(() => {
+    console.log(`runs after the first render and evertime after`)
+    return () => {
+      console.log(`clean up after the effect of previous`)
+    }
+  })
+
+  //to run after every render take out the []
+
   // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, shove the details of the friend in `details` slice of state
+useEffect(() => {
+  //this runs after first render, and then after every render caused by a change in friendID
+  axios.get(`${BASE_URL}/friends/${friendId}?api_key=${API_KEY}`)
+  .then(res => {
+    setDetails(res.data)
+  })
+  .catch (err => {
+    debugger
+  })
+},[friendId])
 
   return (
     <div className='container'>
